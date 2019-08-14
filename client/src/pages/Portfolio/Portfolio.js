a88149a3610f5bfee5c77b8e039db78c1ca8da1d
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-08-13 20:10:13 -0700

    working on portfolio page

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Portfolio.css";
 
 const Portfolio = _ => {
-  return <h1>PORTFOLIO</h1>;
+  return (
+    <>
+      <section>
+        <div className="container">
+          <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">
+            Portfolio
+          </h2>
+          <hr />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Portfolio;
