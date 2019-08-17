71a39b4f0088d629af18d291753b190679e9ce26
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-08-16 21:29:37 -0700

    got styling icons in footer to work

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,22 +4,24 @@ import "./Footer.css";
 const Footer = _ => {
   return (
     <>
+    <div className='footer'>
     <section className='copyright bg-dark py-4 text-center text-white'>
       <h3 id='copyright'>Copyright @ 2019 Kumiko Nomura</h3>
       <div className="container">
         <div className="text-center center-block">
-          <a target="_blank" href="https://www.linkedin.com/in/kumikonomura">
+          <a id='linkedin' target="_blank" href="https://www.linkedin.com/in/kumikonomura">
             <i id='social-fb' class="fa fa-3x fa-linkedin" />
           </a>
-          <a target="_blank" href="https://github.com/kumikonomura">
+          <a id='github' target="_blank" href="https://github.com/kumikonomura">
             <i id='social-tw' class="fa fa-3x fa-github" />
           </a>
-          <a href="mailto:knomurap@gmail.com">
+          <a id='gmail' href="mailto:knomurap@gmail.com">
             <i id='social-gp' className="fa fa-3x fa-envelope" />
           </a>
         </div>
       </div>
       </section>
+      </div>
     </>
     /* // <>
     //   <section id='lab_social_icon_footer className="copyright bg-dark py-4 text-center text-white">
