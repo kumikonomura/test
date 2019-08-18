9080c5572a5fad07481dd7ec7f58f3d2cac5a2ca
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-08-17 21:16:58 -0700

    put in 3 cards in portfolio

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -3,10 +3,12 @@ import Card from "react-bootstrap/Card";
 import "./Portfolio.css";
 import Posme from "../../images/posme.png";
 import Popup from "reactjs-popup";
+import MediaQuery from 'react-responsive'
 
 const Portfolio = _ => {
   return (
     <>
+    <MediaQuery query='(max-width: 1455px)'>
       <section id="portfolio" className="bg-primary">
         <div className="container">
           <h2 className="page-section-heading text-center text-uppercase text-white mb-0">
@@ -14,9 +16,10 @@ const Portfolio = _ => {
           </h2>
         </div>
         {/* </section> */}
-        <div className="container">
+        <div id='projects' className="container">
           <div className="row">
             <div className="col-sm">
+              {/* POSME */}
               <Card className="posme" style={{ width: "18rem" }}>
                 <Card.Img id="image" variant="top" src={Posme} />
                 <Card.Body>
@@ -45,11 +48,72 @@ const Portfolio = _ => {
                 </Card.Body>
               </Card>
             </div>
-            <div className="col-sm">One of three columns</div>
-            <div className="col-sm">One of three columns</div>
+            <div className="col-sm">
+              {/* WHERE'S THE OFFICE */}
+            <Card className="posme" style={{ width: "18rem" }}>
+                <Card.Img id="image" variant="top" src={Posme} />
+                <Card.Body>
+                  <Card.Title id="title">POSME</Card.Title>
+                  <Card.Subtitle id="subtitles" className="mb-2 text-muted">
+                    React, React Hooks, MongoDB, Material UI, Passport, AWS S3
+                  </Card.Subtitle>
+                  <Card.Text>
+                    POSME (Point of Sale Made Easy) is a point-of-sale system
+                    for small businesses to use in their daily operations. It
+                    makes transactions efficient and simple while also tracking
+                    data such as sales and inventory.
+                  </Card.Text>
+                  <Card.Link
+                    target="_blank"
+                    href="https://morning-hamlet-37219.herokuapp.com/"
+                  >
+                    Deployed Site
+                  </Card.Link>
+                  <Card.Link
+                    target="_blank"
+                    href="https://github.com/kumikonomura/posme.git"
+                  >
+                    GitHub Repo
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+
+            </div>
+            <div className="col-sm">
+              {/* PROJECT 3 */}
+            <Card className="posme" style={{ width: "18rem" }}>
+                <Card.Img id="image" variant="top" src={Posme} />
+                <Card.Body>
+                  <Card.Title id="title">POSME</Card.Title>
+                  <Card.Subtitle id="subtitles" className="mb-2 text-muted">
+                    React, React Hooks, MongoDB, Material UI, Passport, AWS S3
+                  </Card.Subtitle>
+                  <Card.Text>
+                    POSME (Point of Sale Made Easy) is a point-of-sale system
+                    for small businesses to use in their daily operations. It
+                    makes transactions efficient and simple while also tracking
+                    data such as sales and inventory.
+                  </Card.Text>
+                  <Card.Link
+                    target="_blank"
+                    href="https://morning-hamlet-37219.herokuapp.com/"
+                  >
+                    Deployed Site
+                  </Card.Link>
+                  <Card.Link
+                    target="_blank"
+                    href="https://github.com/kumikonomura/posme.git"
+                  >
+                    GitHub Repo
+                  </Card.Link>
+                </Card.Body>
+              </Card>
+
+            </div>
           </div>
         </div>
       </section>
+      </MediaQuery>
       {/* <section id="portfolio" className="bg-primary">
         <div className="container">
           <h2 className="page-section-heading text-center text-uppercase text-white mb-0">
